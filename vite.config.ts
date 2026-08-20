b7f27bc68e7d7e5f2fde105325003d8d35a2330a
Dinanath Kumar <freelancingyouth18@gmail.com>
2026-08-20 13:50:16 +0530

    Change base URL for production builds
    update

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,9 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+   base: process.env.FIGMA_PUBLIC_URL
+  ? `${process.env.FIGMA_PUBLIC_URL}/`
+  : '/College_websiteWithvirtulTour/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
